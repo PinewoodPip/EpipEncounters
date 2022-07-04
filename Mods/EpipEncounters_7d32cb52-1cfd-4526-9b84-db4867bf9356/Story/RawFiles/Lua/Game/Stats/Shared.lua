0afe6a0af5d5b1c9b4db3a52cb88eaf51208cf0d
Kamil <lekamilthemage@gmail.com>
2022-07-04 20:11:03 +0200

    Remove old GetSkillAPCost 'fix'

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Game/Stats/Shared.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Game/Stats/Shared.lua
@@ -6,60 +6,6 @@ Game.Stats = {
 local Stats = Game.Stats
 Epip.InitializeLibrary("Stats", Stats)
 
-local ElementalAffinityAiFlags = {
-    Fire = {0x1000000, 0x20000000},
-    Water = { 0x2000000 },
-    Air = { 0x40000000000000 },
-    Earth = { 0x10000000, 0x8000000 },
-    Death = { 0x4000000 },
-    Sulfurology = { 0x200000000 }
-}
-
-function bitand(a, b)
-    local result = 0
-    local bitval = 1
-    while a > 0 and b > 0 do
-      if a % 2 == 1 and b % 2 == 1 then -- test the rightmost bits
-          result = result + bitval      -- set the current bit
-      end
-      bitval = bitval * 2 -- shift left
-      a = math.floor(a/2) -- shift right
-      b = math.floor(b/2)
-    end
-    return result
-end
-
-function GetSkillAPCost(skill, character, grid, position, radius)
-    local baseAP = skill.ActionPoints
-    if character == nil or baseAP <= 0 then
-        return baseAP, false
-    end
-    
-    local ability = skill.Ability
-    local elementalAffinity = false
-    if ability ~= "None" and baseAP > 1 and character.TALENT_ElementalAffinity and grid ~= nil and position ~= nil and radius ~= nil then
-        local aiFlags = ElementalAffinityAiFlags[ability]
-        if aiFlags ~= nil then
-            elementalAffinity = grid:SearchForCell(position[1], position[3], radius, aiFlags[1], -1.0)
-
-            local cell = grid:GetCellInfo(position[1], position[3])
-            
-            elementalAffinity = bitand(cell.Flags, aiFlags[1]) > 0
-
-            if elementalAffinity then
-                baseAP = baseAP - 1
-            end
-        end
-    end
-
-    local characterAP = 1
-    if skill.Requirement ~= "None" and skill.OverrideMinAP == "No" then
-        characterAP = Game.Math.GetCharacterWeaponAPCost(character)
-    end
-
-    return math.max(characterAP, baseAP), elementalAffinity
-end
-
 ---Returns whether char meets the requirements for a stat object to be used.
 ---@param char Character
 ---@param skillID string
@@ -80,7 +26,7 @@ function Stats.MeetsRequirements(char, statID, isItem)
 
     --- AP cost
     if not isItem then
-        local apCost,EA = GetSkillAPCost(data, char.Stats, Ext.Entity.GetAiGrid(), char.Translate, 1)
+        local apCost,EA = Game.Math.GetSkillAPCost(data, char.Stats, Ext.Entity.GetAiGrid(), char.Translate, 1)
 
         -- Consider APCostBoost
         local extraApCost = Stats.CountStat(char.Stats, "APCostBoost")
