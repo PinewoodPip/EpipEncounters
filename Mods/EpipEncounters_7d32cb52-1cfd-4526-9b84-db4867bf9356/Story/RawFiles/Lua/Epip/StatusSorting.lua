9b3fcef2d6ef3e903d270a5aed3247a9066f272b
Kamil <lekamilthemage@gmail.com>
2022-07-16 13:27:05 +0200

    StatusSorting: grey out unavailable options

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/StatusSorting.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/StatusSorting.lua
@@ -11,6 +11,10 @@ local StatusSorting = {
         ["PlayerInfo_Filter_SourceGen"] = {"^AMER_SOURCEGEN_DISPLAY_%d+$"},
         ["PlayerInfo_Filter_BatteredHarried"] = {Text.PATTERNS.STATUSES.BATTERED, Text.PATTERNS.STATUSES.HARRIED},
     },
+    SUB_SETTINGS = {
+        ["PlayerInfo_Filter_SourceGen"] = "Checkbox",
+        ["PlayerInfo_Filter_BatteredHarried"] = "Checkbox",
+    },
     USE_LEGACY_EVENTS = false,
     Events = {
         ---@type SubscribableEvent<StatusSortingFeature_Event_ShouldFilterStatus>
@@ -60,5 +64,30 @@ StatusSorting.Events.ShouldFilterStatus:Subscribe(function (e)
                 end
             end
         end
+    end
+end)
+
+local function UpdateOptionAvailability(sortingEnabled)
+    if sortingEnabled == nil then sortingEnabled = OptionsSettings.GetOptionValue("EpipEncounters", "PlayerInfo_EnableSortingFiltering") end
+
+    for id,elementType in pairs(StatusSorting.SUB_SETTINGS) do
+        if not sortingEnabled then
+            OptionsSettings.SetOptionValue("EpipEncounters", id, OptionsSettings.GetOptionData(id).DefaultValue)
+        end
+        
+        OptionsSettings.SetElementEnabled(id, sortingEnabled, elementType)
+    end
+end
+
+OptionsSettings:RegisterListener("CheckboxClicked", function(element, active)
+    if element and element.ID == "PlayerInfo_EnableSortingFiltering" then
+        UpdateOptionAvailability(active)
+    end
+end)
+
+-- Enable/disable the sorting settings depending on whether sorting itself is enabled.
+OptionsSettings.Events.TabRendered:RegisterListener(function (customTab, _)
+    if customTab and customTab.Mod == "EpipEncounters" then
+        UpdateOptionAvailability()
     end
 end)
