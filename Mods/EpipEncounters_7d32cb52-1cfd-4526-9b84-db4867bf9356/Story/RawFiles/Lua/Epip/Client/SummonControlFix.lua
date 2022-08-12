52681db3671f287f4ba183fe16605bb88ce114c4
Kamil <lekamilthemage@gmail.com>
2022-08-12 09:31:44 +0200

    SummonControlFix: also apply the fix to killing non-controlled summons

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/SummonControlFix.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/SummonControlFix.lua
@@ -18,19 +18,14 @@ PlayerInfo.Events.ActiveCharacterChanged:Subscribe(function (e)
 
     -- Ignore manual character changes
     if newChar and prevChar and not e.Manual then
-        
-        -- Only restore control if switching from a character to summon. We do not check if the char is the summoner as this bug can cause the control to be switched to other char's summons as well.
-        if newChar.HasOwner and not prevChar.HasOwner then
-            local summonerHandle = prevChar.Handle
 
-            -- And only within combat if the summon does not have its turn
-            if Client.IsInCombat() and not Client.IsActiveCombatant() then
-                local summoner = Character.Get(summonerHandle)
+        -- Only swap if it is not the new char's turn
+        if Client.IsInCombat() and not Client.IsActiveCombatant() then
+            local previousCharacter = Character.Get(prevChar.Handle)
 
-                Fix:DebugLog("Restoring control to summoner", summoner.DisplayName)
+            Fix:DebugLog("Restoring control to previous character", previousCharacter.DisplayName)
 
-                PlayerInfo.SelectCharacter(summoner.Handle)
-            end
+            PlayerInfo.SelectCharacter(previousCharacter.Handle)
         end
     end
 end)
