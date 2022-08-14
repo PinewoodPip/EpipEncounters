b421e0b14d1488098f2e239ba87b2aef049647f1
Kamil <lekamilthemage@gmail.com>
2022-08-14 08:19:16 +0200

    SummonControlFix: fix other ways of switching chars being bugged
    Fixes switching chars in character sheet as well as through the keybinds.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/SummonControlFix.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/SummonControlFix.lua
@@ -1,9 +1,15 @@
 
 local PlayerInfo = Client.UI.PlayerInfo
+local CharacterSheet = Client.UI.CharacterSheet
 
----@type Feature
+---@class Feature_SummonControlFix : Feature
 local Fix = {
+    ignoreNextSelect = false,
 
+    CHARACTER_SWITCH_INPUT_EVENTS = {
+        [218] = true,
+        [219] = true,
+    }
 }
 Epip.AddFeature("SummonControlFix", "SummonControlFix", Fix)
 Fix:Debug()
@@ -12,12 +18,24 @@ Fix:Debug()
 -- EVENT LISTENERS
 ---------------------------------------------
 
+CharacterSheet:RegisterCallListener("selectCharacter", function(_, _)
+    Fix.ignoreNextSelect = true
+end)
+
+Ext.Events.InputEvent:Subscribe(function(ev)
+    local inputEvent = ev.Event
+
+    if Fix.CHARACTER_SWITCH_INPUT_EVENTS[inputEvent.EventId] then
+        Fix.ignoreNextSelect = true
+    end
+end)
+
 PlayerInfo.Events.ActiveCharacterChanged:Subscribe(function (e)
     local newChar = e.NewCharacter
     local prevChar = e.PreviousCharacter
 
     -- Ignore manual character changes
-    if newChar and prevChar and not e.Manual then
+    if newChar and prevChar and not e.Manual and not Fix.ignoreNextSelect then
 
         -- Only swap if it is not the new char's turn
         if Client.IsInCombat() and not Client.IsActiveCombatant() then
@@ -28,4 +46,6 @@ PlayerInfo.Events.ActiveCharacterChanged:Subscribe(function (e)
             PlayerInfo.SelectCharacter(previousCharacter.Handle)
         end
     end
+
+    Fix.ignoreNextSelect = false
 end)
