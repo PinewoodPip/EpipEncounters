065c132baa5ae47cbeed26bbe2024518c0341028
Kamil <lekamilthemage@gmail.com>
2022-09-14 19:11:59 +0200

    Update GenericUI example script

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Examples/GenericUI.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Examples/GenericUI.lua
@@ -2,28 +2,35 @@
 local Generic = Client.UI.Generic
 local SpinnerPrefab = Generic.GetPrefab("GenericUI_Prefab_Spinner")
 
+-- Creates the UI
 ExampleUI = Generic.Create("PIP_ExampleUI")
 
+-- Adds a background element
 local background = ExampleUI:CreateElement("Background", "GenericUI_Element_TiledBackground")
 background:SetBackground("RedPrompt", 400, 400) -- 400x400 panel size
 
+-- Adds a button
 local closeButton = background:AddChild("CloseButton", "GenericUI_Element_Button")
-closeButton:SetType("Close")
-closeButton:SetPositionRelativeToParent("TopRight", -20, 20)
+closeButton:SetType("Close") -- Sets the appearance
+closeButton:SetPositionRelativeToParent("TopRight", -20, 20) -- Puts the button in the top right corner of the parent element (Background) with a 20px offset on both axes
 
--- Events can be registered from the elements, with basic ones like MouseOver, MouseUp also being available
-closeButton.Events.Pressed:Subscribe(function (_) -- Close the UI when the button is pressed
+-- Events can be registered from the elements, with basic ones like MouseOver, MouseUp also being available in addition to the high-level ones implemented by each element type. Pressed is akin to MouseUp, but only fires if the button is not disabled.
+closeButton.Events.Pressed:Subscribe(function (_)
+    -- Close the UI when the button is pressed
     ExampleUI:Hide()
 end)
 
-local header = background:AddChild("Header", "GenericUI_Element_Text")
+-- Adds some text
+local header = background:AddChild("MyHeader", "GenericUI_Element_Text")
 header:SetText(Text.Format("My UI", {Color = Color.BLACK}))
 header:SetSize(400, 50)
-header:SetPositionRelativeToParent("Top", 0, 60) -- Sets the header's position to the top of panel, with a 60px offset from the top
+header:SetPositionRelativeToParent("Top", 0, 60) -- Sets the header's position to the top center of the panel, with a 60px offset from the top
 
+-- VerticalList is the equivalent of Larian's listDisplay; HorizontalList and Grid are also available
 local contentList = background:AddChild("ContentList", "GenericUI_Element_VerticalList")
 contentList:SetElementSpacing(10)
 
+-- Adding children to a list-like element automatically positions them, similar to listDisplay.addElement()
 local dropdown = contentList:AddChild("Dropdown", "GenericUI_Element_ComboBox")
 dropdown:SetOptions({
     {ID = "Option1", Label = "Option 1"},
@@ -33,11 +40,11 @@ dropdown:SetOptions({
 dropdown:SelectOption("Option2")
 
 -- Prefabs can be created to work with groups of elements in an abstracted and reusable manner, simulating custom sprites
--- The Spinner prefab implements a spinner form element using Text + Button elements
-local spinner = SpinnerPrefab.Create(ExampleUI, "MySpinner", contentList, "My Label", 0, 10, 1) -- Min value 0, max value 10, step 1
+-- The Spinner prefab implements a spinner form control using Text + Button elements
+local spinner = SpinnerPrefab.Create(ExampleUI, "MySpinner", contentList, Text.Format("My Spinner", {Color = Color.BLACK}), 0, 10, 1) -- Min value 0, max value 10, step 1; parented to contentList
 spinner:SetSize(270, 30)
 spinner:SetValue(3)
-spinner.Events.ValueChanged:Subscribe(function (ev) -- With custom, contextual events, too
+spinner.Events.ValueChanged:Subscribe(function (ev) -- Prefabs can implement their own contextual events
     print("Spinner value changed", ev.Value)
 end)
 
@@ -46,6 +53,8 @@ contentList:SetPositionRelativeToParent("Center")
 local acceptButton = background:AddChild("AcceptButton", "GenericUI_Element_Button")
 acceptButton:SetText(Text.Format("Accept", {Size = 17}), 5)
 acceptButton:SetPositionRelativeToParent("Bottom", 0, -50)
+
+-- Enable the accept button when a specific dropdown option is chosen
 dropdown.Events.OptionSelected:Subscribe(function (ev)
     print("Dropdown option selected", ev.Option.ID)
 
@@ -55,6 +64,9 @@ end)
 -- Direct access to UIObject and MovieClip is available
 print(ExampleUI:GetUI())
 print(acceptButton:GetMovieClip())
+
+-- GenericUI instances inherit from the same class as built-in UIs in Epip
+print(ExampleUI:IsFlagged("OF_PlayerInput1"))
 
 -- Elements can be referenced by their ID even if not explicitly exposed by original modder, and modified at any time
 print(ExampleUI:GetElementByID("AcceptButton"))
