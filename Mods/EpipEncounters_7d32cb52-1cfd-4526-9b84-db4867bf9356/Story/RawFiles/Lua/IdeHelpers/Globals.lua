1bef43dfd08fb8db90d804e9661c82c3a458bda5
Kamil <lekamilthemage@gmail.com>
2022-11-02 18:18:30 +0100

    Add IDE annotation file for globals

---
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/IdeHelpers/Globals.lua
@@ -0,0 +1,23 @@
+
+---------------------------------------------
+-- DEBUG GLOBAL FUNCTIONS
+---------------------------------------------
+
+---@deprecated Use Feature:Dump() instead.
+---@param value any
+function _D(value) end
+
+---@deprecated Use the Feature logging functions instead.
+function print(...) end
+
+---@deprecated Not intended for production! Use Client.GetCharacter() instead.
+---@return Character
+function _C() end
+
+---@deprecated Not intended for production!
+---@return EsvItem
+function _W() end
+
+---@deprecated Not intended for production! Use Client.UI.Examine.GetCharacter() instead.
+---@return EclCharacter
+function _E() end
