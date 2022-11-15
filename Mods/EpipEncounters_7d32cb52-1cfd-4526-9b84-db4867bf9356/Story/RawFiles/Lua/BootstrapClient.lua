18d6caf21747e2a973a11a95646b22313e7b022f
Kamil <lekamilthemage@gmail.com>
2022-11-15 18:17:14 +0100

    Add fix for astrologer gaze range tooltip
    The game mistakenly says it increases the range of cone skills.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/BootstrapClient.lua
@@ -308,6 +308,7 @@ LOAD_ORDER = {
             "Epip/TooltipAdjustments/Client_WeaponRangeDeltamod.lua",
             "Epip/TooltipAdjustments/Client_DamageTypeDeltamods.lua",
             "Epip/TooltipAdjustments/Client_SimpleTooltips.lua",
+            "Epip/TooltipAdjustments/Client_AstrologerFix.lua",
         },
     },
 
---
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/TooltipAdjustments/Client_AstrologerFix.lua
@@ -0,0 +1,52 @@
+
+---------------------------------------------
+-- Fixes skill tooltips for cone skills saying that their range
+-- is affected by Astrologer's Gaze (it is not!)
+---------------------------------------------
+
+local Tooltip = Client.Tooltip
+
+local AstrologerFix = {
+    ---@type table<string, true> Skill archetypes affected by the bug.
+    AFFECTED_ARCHETYPES = {
+        Cone = true,
+    },
+    RANGE_BONUS = 200, -- Range bonus from the talent, in centimeters.
+}
+Epip.RegisterFeature("TooltipAdjustments_AstrologerFix", AstrologerFix)
+
+---------------------------------------------
+-- METHODS
+---------------------------------------------
+
+---@param char EclCharacter
+---@param skillData StatsLib_StatsObject_SkillData
+---@return boolean
+function AstrologerFix.IsAffected(char, skillData)
+    local skillType = skillData.SkillType
+
+    return char.Stats.TALENT_FaroutDude and AstrologerFix.AFFECTED_ARCHETYPES[skillType] == true
+end
+
+---------------------------------------------
+-- EVENT LISTENERS
+---------------------------------------------
+
+-- Listen for skill tooltips to check if they're affected.
+Tooltip.Hooks.RenderSkillTooltip:Subscribe(function (ev)
+    local skill = Stats.Get("SkillData", ev.SkillID)
+    local char = Client.GetCharacter()
+
+    if skill and AstrologerFix.IsAffected(char, skill) and AstrologerFix:IsEnabled() then
+        local element = ev.Tooltip:GetFirstElement("SkillRange")
+        local range = element.Value:match("(%d+%.?%d*)m$")
+
+        if range then
+            local newRange = tonumber(range) - AstrologerFix.RANGE_BONUS/100
+
+            AstrologerFix:DebugLog("Found bugged tooltip. Old and new range:", range, newRange)
+
+            element.Value = string.format("%sm", Text.RemoveTrailingZeros(newRange))
+        end
+    end
+end)
