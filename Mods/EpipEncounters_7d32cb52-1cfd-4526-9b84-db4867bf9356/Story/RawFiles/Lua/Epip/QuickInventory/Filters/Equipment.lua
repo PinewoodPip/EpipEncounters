c7bee8e5d638ab0519e63bf15994594f953b8812
Kamil <lekamilthemage@gmail.com>
2023-02-03 17:46:35 +0100

    QuickInventory: sort skillbooks by school, move settings to filter scripts

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/QuickInventory/Filters/Equipment.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/QuickInventory/Filters/Equipment.lua
@@ -1,6 +1,47 @@
 
+local CommonStrings = Text.CommonStrings
+
 ---@class Feature_QuickInventory
 local QuickInventory = Epip.GetFeature("Feature_QuickInventory")
+
+---------------------------------------------
+-- SETTINGS
+---------------------------------------------
+
+QuickInventory.Settings.ItemSlot = QuickInventory:RegisterSetting("ItemSlot", {
+    Type = "Choice",
+    Name = CommonStrings.ItemSlot,
+    DefaultValue = "Helmet",
+    ---@type SettingsLib_Setting_Choice_Entry[]
+    Choices = {
+        {ID = "Any", NameHandle = CommonStrings.Any.Handle},
+        {ID = "Helmet", NameHandle = CommonStrings.Helmet.Handle},
+        {ID = "Breast", NameHandle = CommonStrings.Breast.Handle},
+        {ID = "Leggings", NameHandle = CommonStrings.Leggings.Handle},
+        {ID = "Weapon", NameHandle = CommonStrings.Weapon.Handle},
+        {ID = "Shield", NameHandle = CommonStrings.Shield.Handle},
+        {ID = "Ring", NameHandle = CommonStrings.Ring.Handle},
+        {ID = "Boots", NameHandle = CommonStrings.Boots.Handle},
+        {ID = "Belt", NameHandle = CommonStrings.Belt.Handle},
+        {ID = "Amulet", NameHandle = CommonStrings.Amulet.Handle},
+    },
+})
+
+QuickInventory.Settings.WeaponSubType = QuickInventory:RegisterSetting("WeaponSubType", {
+    Type = "Choice",
+    Name = CommonStrings.WeaponType,
+    DefaultValue = "Any",
+    ---@type SettingsLib_Setting_Choice_Entry[]
+    Choices = {
+        {ID = "Any", NameHandle = Text.CommonStrings.Any.Handle},
+        {ID = "Sword", NameHandle = Text.CommonStrings.Sword.Handle},
+        {ID = "Axe", NameHandle = Text.CommonStrings.Axe.Handle},
+        {ID = "Club", NameHandle = Text.CommonStrings.Club.Handle},
+        {ID = "Staff", NameHandle = Text.CommonStrings.Staff.Handle},
+        {ID = "Knife", NameHandle = Text.CommonStrings.Knife.Handle},
+        {ID = "Spear", NameHandle = Text.CommonStrings.Spear.Handle},
+    },
+})
 
 ---------------------------------------------
 -- EVENT LISTENERS
--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/QuickInventory/Filters/Skillbooks.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/QuickInventory/Filters/Skillbooks.lua
@@ -1,6 +1,43 @@
 
+local CommonStrings = Text.CommonStrings
+
 ---@class Feature_QuickInventory
 local QuickInventory = Epip.GetFeature("Feature_QuickInventory")
+
+---------------------------------------------
+-- SETTINGS
+---------------------------------------------
+
+QuickInventory.Settings.LearntSkillbooks = QuickInventory:RegisterSetting("LearntSkillbooks", {
+    Type = "Boolean",
+    Name = QuickInventory.TranslatedStrings.ShowLearntSkills_Name,
+    DefaultValue = false,
+})
+
+QuickInventory.Settings.SkillbookSchool = QuickInventory:RegisterSetting("SkillbookSchool", {
+    Type = "Choice",
+    Name = CommonStrings.AbilitySchool,
+    DefaultValue = "Any",
+    ---@type SettingsLib_Setting_Choice_Entry[]
+    Choices = { -- Ordered by appearance in skillbook UI
+        {ID = "Any", NameHandle = Text.CommonStrings.Any.Handle},
+        {ID = "Warrior", NameHandle = Text.CommonStrings.Warfare.Handle},
+        {ID = "Ranger", NameHandle = Text.CommonStrings.Huntsman.Handle},
+        {ID = "Rogue", NameHandle = Text.CommonStrings.Scoundrel.Handle},
+        {ID = "Fire", NameHandle = Text.CommonStrings.Pyrokinetic.Handle},
+        {ID = "Water", NameHandle = Text.CommonStrings.Hydrosophist.Handle},
+        {ID = "Air", NameHandle = Text.CommonStrings.Aerotheurge.Handle},
+        {ID = "Earth", NameHandle = Text.CommonStrings.Geomancer.Handle},
+        {ID = "Death", NameHandle = Text.CommonStrings.Necromancer.Handle},
+        {ID = "Summoning", NameHandle = Text.CommonStrings.Summoning.Handle},
+        {ID = "Polymorph", NameHandle = Text.CommonStrings.Polymorph.Handle},
+    },
+})
+local SCHOOL_TO_PRIORITY = {}
+local skillBookSchoolSetting = QuickInventory.Settings.SkillbookSchool ---@type SettingsLib_Setting_Choice
+for i,choice in ipairs(skillBookSchoolSetting.Choices) do
+    SCHOOL_TO_PRIORITY[choice.ID] = i
+end
 
 ---------------------------------------------
 -- EVENT LISTENERS
@@ -44,4 +81,19 @@ QuickInventory.Hooks.IsItemVisible:Subscribe(function (ev)
     end
     
     ev.Visible = visible
+end)
+
+-- Sort by school.
+QuickInventory.Hooks.SortItems:Subscribe(function (ev)
+    if QuickInventory:GetSettingValue(QuickInventory.Settings.ItemCategory) == "Skillbooks" then
+        local action1 = Item.GetUseActions(ev.ItemA, "SkillBook")[1] ---@type SkillBookActionData
+        local action2 = Item.GetUseActions(ev.ItemB, "SkillBook")[1] ---@type SkillBookActionData
+
+        if action1 and action2 then
+            local stat1, stat2 = Stats.Get("SkillData", action1.SkillID), Stats.Get("SkillData", action2.SkillID)
+
+            ev.Result = (SCHOOL_TO_PRIORITY[stat1.Ability] or -1) < (SCHOOL_TO_PRIORITY[stat2.Ability] or -1)
+            ev:StopPropagation()
+        end
+    end
 end)
