e8e55b90ca04fa0d0840b6b6be7cb11df47c47e9
Kamil <lekamilthemage@gmail.com>
2023-02-15 19:11:24 +0100

    AutoUnlockInventory: try to fix the crash again

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/AutoUnlockInventory/Client.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/AutoUnlockInventory/Client.lua
@@ -18,9 +18,10 @@ end
 -- EVENT LISTENERS
 ---------------------------------------------
 
--- Listen for inventory lock being toggled - we synchronize this manually, as the engine seems to struggle at this, possibly due to use screwing with it.
+-- Listen for inventory lock being toggled - we synchronize this manually, as the engine seems to struggle at this.
+-- The extra "scripted" parameter is just coming from lua. The UI is not modified.
 Inventory:RegisterCallListener("lockInventory", function (ev, flashCharacterHandle, state, isScripted)
-    if AutoUnlock:IsEnabled() and not isScripted then -- TODO investigate if this issue occurs in vanilla as well.
+    if not isScripted then -- Needs to be done even for clients with the setting disabled.
         AutoUnlock:DebugLog("Synching lock/unlock request")
 
         Net.PostToServer("EPIPENCOUNTERS_ToggleInventoryLock", {
@@ -35,31 +36,24 @@ end)
 -- Listen for being notified of other characters wanting to have their inventory unlocked.
 Net.RegisterListener("EPIPENCOUNTERS_ToggleInventoryLock", function (payload)
     local char = Character.Get(payload.NetID)
-    local inventoryWasVisible = Inventory:IsVisible()
 
     AutoUnlock:DebugLog("Received request to unlock inventory of " .. char.DisplayName)
 
-    -- Need to show the inventory momentarily to avoid a mysterious rare crash that results from v56 inventory sync fixes.
-    Inventory:Show()
-
     Inventory:ExternalInterfaceCall("lockInventory", Ext.UI.HandleToDouble(char.Handle), payload.State, true) -- We add an extra parameter to distinguish scripted calls.
-
-    -- Only hide the inventory if it was not previously visible.
-    if not inventoryWasVisible then
-        Inventory:Hide()
-    end
 end)
 
 -- Request inventory to be opened when the game is ready.
-GameState.Events.GameReady:Subscribe(function (_)
+GameState.Events.ClientReady:Subscribe(function (_)
     if AutoUnlock:IsEnabled() then
-        AutoUnlock:DebugLog("Requesting unlock")
-
-        local characters = Client.UI.PlayerInfo.GetCharacters(true)
-        for _,char in ipairs(characters) do
-            AutoUnlock:DebugLog("Unlocking inventory of " .. char.DisplayName)
-
-            Net.PostToServer("EPIPENCOUNTERS_ToggleInventoryLock", {NetID = char.NetID, State = false})
-        end
+        Timer.Start(2, function (_) -- Possibly unnecessary?
+            AutoUnlock:DebugLog("Requesting unlock")
+    
+            local characters = Client.UI.PlayerInfo.GetCharacters(true)
+            for _,char in ipairs(characters) do
+                AutoUnlock:DebugLog("Unlocking inventory of " .. char.DisplayName)
+    
+                Net.PostToServer("EPIPENCOUNTERS_ToggleInventoryLock", {NetID = char.NetID, State = false})
+            end
+        end)
     end
 end)
