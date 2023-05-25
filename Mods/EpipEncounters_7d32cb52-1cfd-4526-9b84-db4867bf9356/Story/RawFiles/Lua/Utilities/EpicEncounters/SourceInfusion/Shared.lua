6c4e0771aabeca685ba9df398459c361e7ad9b84
Kamil <lekamilthemage@gmail.com>
2023-05-25 20:35:08 +0200

    SourceInfusionLib: add source gen display statuses

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/EpicEncounters/SourceInfusion/Shared.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/EpicEncounters/SourceInfusion/Shared.lua
@@ -14,6 +14,15 @@ local SourceInfusion = {
         9,
     },
     MAX_INFUSION_LEVEL = 3,
+    SOURCE_GENERATION_DISPLAY_STATUSES = {
+        "AMER_SOURCEGEN_DISPLAY_1",
+        "AMER_SOURCEGEN_DISPLAY_2",
+        "AMER_SOURCEGEN_DISPLAY_3",
+        "AMER_SOURCEGEN_DISPLAY_4",
+        "AMER_SOURCEGEN_DISPLAY_5",
+        "AMER_SOURCEGEN_DISPLAY_6",
+        "AMER_SOURCEGEN_DISPLAY_7",
+    },
 }
 Epip.InitializeLibrary("EpicEncounters_SourceInfusion", SourceInfusion)
 EpicEncounters.SourceInfusion = SourceInfusion
@@ -45,4 +54,16 @@ end
 ---@return boolean
 function SourceInfusion.IsPreparingInfusion(char)
     return SourceInfusion.GetPreparedInfusionLevel(char) > 0
+end
+
+---Returns the display status ID
+---for an amount of Source Generation.
+---@param amount integer
+---@return string? `nil` if amount is <= 0.
+function SourceInfusion.GetGenerationDisplayStatus(amount)
+    -- Source Gen beyond the maximum uses the last available status
+    amount = math.min(amount or 0, #SourceInfusion.SOURCE_GENERATION_DISPLAY_STATUSES)
+    local status = SourceInfusion.SOURCE_GENERATION_DISPLAY_STATUSES[amount]
+
+    return status
 end
