ead0a7dd78fcaba41d91a3f100704f18c2842b3e
Kamil <lekamilthemage@gmail.com>
2023-06-07 18:55:20 +0200

    ItemLib: strip template names in GetPartyTemplateCount

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/Item/Client.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/Item/Client.lua
@@ -14,9 +14,10 @@ end
 ---@param template string
 ---@return number
 function Item.GetPartyTemplateCount(template)
+    local templateGUID = template:match(Text.PATTERNS.GUID)
     local count = 0
     local predicate = function(item)
-        return item.RootTemplate.Id == template
+        return item.RootTemplate.Id == templateGUID
     end
 
     for _,player in ipairs(Character.GetPartyMembers(Client.GetCharacter())) do
