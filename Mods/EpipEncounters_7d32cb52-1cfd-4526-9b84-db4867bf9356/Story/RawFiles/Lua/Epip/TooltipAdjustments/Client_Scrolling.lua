cb96281b2d27f5c6181329fb6c173d6bcc71f6de
Kamil <lekamilthemage@gmail.com>
2023-09-17 14:45:14 +0200

    TooltipScrolling: extract TSKs
    Also removed unused code.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/TooltipAdjustments/Client_Scrolling.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/TooltipAdjustments/Client_Scrolling.lua
@@ -7,10 +7,25 @@ local TooltipScrolling = {
     tooltipExists = false,
     originalTooltipPosition = {0, 0}, ---@type Vector2D
 
+    ----@type table<string, TextLib_TranslatedString>
+    TranslatedStrings = {
+        Label_Enabled = {
+           Handle = "h89b8939cga077g4a2egb700g3d22e477c3f6",
+           Text = "Tooltip scrolling enabled.",
+           ContextDescription = "Shown when enabling scrolling",
+        },
+        Label_Disabled = {
+           Handle = "h53c5d893gb6f2g4e4bg94aega9462369df7d",
+           Text = "Tooltip scrolling disabled.",
+           ContextDescription = "Shown when disabling scrolling.",
+        },
+    },
+
     INPUT_ACTION = "EpipEncounters_ScrollTooltip",
     SCROLLING_STEP = 40,
 }
 Epip.RegisterFeature("TooltipScrolling", TooltipScrolling)
+local TSK = TooltipScrolling.TranslatedStrings
 
 ---------------------------------------------
 -- METHODS
@@ -23,9 +38,7 @@ function TooltipScrolling.Toggle(active, notify)
         Client.UI.Input.SetMouseWheelBlocked(active) -- We do not block the mouse wheel motion directly as that breaks the middle click events from firing, apparently - and this is the default binding for this feature.
 
         if notify then
-            local message = "Tooltip scrolling enabled."
-            if not active then message = "Tooltip scrolling disabled." end
-
+            local message = active and TSK.Label_Enabled:GetString() or TSK.Label_Disabled:GetString()
             Client.UI.Notification.ShowNotification(message)
         end
 
@@ -69,19 +82,12 @@ Client.Input.Events.KeyPressed:Subscribe(function (ev)
 end)
 
 -- Listen for tooltips being created; this listener is fired as late as possible to check if any other listeners has chosen to block the tooltip.
--- Client.Tooltip.Hooks.RenderFormattedTooltip:Subscribe(function (ev)
---     if not ev.Prevented then
---         TooltipScrolling.tooltipExists = true
-
---         TooltipScrolling:DebugLog("Tooltip now exists")
---     end
--- end, {Priority = -1000})
 TooltipUI:RegisterInvokeListener("addFormattedTooltip", function (_)
     TooltipScrolling.tooltipExists = true
 
     TooltipScrolling:DebugLog("Tooltip now exists")
 
-    -- It's possible for a tooltip being rendered without removeTooltip being called for a previous one.
+    -- It's possible for a tooltip to be rendered without removeTooltip being called for a previous one.
     if TooltipScrolling.IsActive() then
         TooltipScrolling.Toggle(false, false)
     end
