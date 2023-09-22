79c9f8cbffa8f4f68acc7531a17ddf4314e11f25
Kamil <lekamilthemage@gmail.com>
2023-09-22 18:41:36 +0200

    AutoIdentify: identify whole party inventory when setting is toggled on
    This includes on load. Hopefully not very impactful on performance.
    Also updated the script a bit and possibly fixed "on template added" not working.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Server/AutoIdentify.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Server/AutoIdentify.lua
@@ -25,14 +25,25 @@ function AutoIdentify.IsEnabled()
     return (state > AutoIdentify.STATES.DISABLED) or AutoIdentify.forceEnable
 end
 
+---Attempts to identify an item.
+---The item will only be identified if the feature is enabled and the item is eligible.
+---@param item EsvItem
+---@return boolean -- Whether the item was identified.
 function AutoIdentify.ProcessItem(item)
+    local identified = false
     if item.Stats and AutoIdentify.IsEnabled() then
         if AutoIdentify.CanIdentify(item) then
             Osi.NRD_ItemSetIdentified(item.MyGuid, 1)
+            identified = true
         end
     end
+    return identified
 end
 
+---Returns whether an item can be auto-identified.
+---Ignores whether the feature is enabled.
+---@param item EsvItem
+---@return boolean
 function AutoIdentify.CanIdentify(item)
     local state = AutoIdentify.state
     local canIdentify = state == AutoIdentify.STATES.ALWAYS or AutoIdentify.forceEnable
@@ -50,20 +61,32 @@ function AutoIdentify.CanIdentify(item)
     return canIdentify
 end
 
-Ext.RegisterListener("TreasureItemGenerated", function(item)
-    AutoIdentify.ProcessItem(item)
+-- Auto-identify items on generation.
+Ext.Events.TreasureItemGenerated:Subscribe(function (ev)
+    AutoIdentify.ProcessItem(ev.Item)
 end)
 
-Ext.Osiris.RegisterListener("ItemTemplateAddedToCharacter", 3, "after", function(template, item, char)
-    AutoIdentify.ProcessItem(item)
+-- Auto-identify items moved to inventories.
+Osiris.RegisterSymbolListener("ItemTemplateAddedToCharacter", 3, "after", function(_, itemGUID, _)
+    AutoIdentify.ProcessItem(Item.Get(itemGUID))
 end)
 
 Settings.Events.SettingValueChanged:Subscribe(function (ev)
     local setting = ev.Setting
-
     if setting.ModTable == "EpipEncounters" and setting.ID == "AutoIdentify" then
         AutoIdentify.state = ev.Value
 
         Utilities.Log("AutoIdentify", "Toggled autoidentify to state " .. AutoIdentify.state)
+
+        -- Auto-identify all items in the party inventory when the feature is enabled.
+        if AutoIdentify:IsEnabled() then
+            local host = Character.Get(Osi.CharacterGetHostCharacter())
+            local items = Item.GetItemsInPartyInventory(host, function (item)
+                return item.Stats and not Item.IsIdentified(item)
+            end, true)
+            for _,item in ipairs(items) do
+                AutoIdentify.ProcessItem(item)
+            end
+        end
     end
 end)
