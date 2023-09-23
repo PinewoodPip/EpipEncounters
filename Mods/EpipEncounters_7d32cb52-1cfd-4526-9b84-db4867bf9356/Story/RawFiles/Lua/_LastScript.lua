a0a3350272638caab054ba62671bb4f1a3b75610
Kamil <lekamilthemage@gmail.com>
2023-09-23 18:01:36 +0200

    Catch errors during Library:__Setup()

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/_LastScript.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/_LastScript.lua
@@ -6,12 +6,17 @@
 Ext.Events.SessionLoaded:Subscribe(function(_)
     for _,lib in ipairs(Epip._FeatureRegistrationOrder) do
         if lib.IsEnabled ~= nil and lib:IsEnabled() then -- TODO move everything to lib system
-            lib:__Setup()
+            local success, msg = pcall(function ()
+                lib:__Setup()
 
-            if lib:IsDebug() and Epip.IsDeveloperMode(true) then
-                Timer.Start(2, function (_)
-                    lib:__Test()
-                end)
+                if lib:IsDebug() and Epip.IsDeveloperMode(true) then
+                    Timer.Start(2, function (_)
+                        lib:__Test()
+                    end)
+                end
+            end)
+            if not success then
+                Ext.Utils.PrintError("Error during _LastScript:", msg)
             end
         end
     end
