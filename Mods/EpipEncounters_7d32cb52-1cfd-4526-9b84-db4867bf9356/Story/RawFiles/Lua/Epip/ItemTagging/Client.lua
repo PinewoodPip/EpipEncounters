e1f0675adfb64b27a2682ca8e271b11ce1da520e
Kamil <lekamilthemage@gmail.com>
2023-10-01 13:42:48 +0200

    ItemTagging: fix inconsistent tooltip, extract TSKs

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ItemTagging/Client.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ItemTagging/Client.lua
@@ -1,6 +1,7 @@
 
 ---@class Features.ItemTagging
 local ItemTagging = Epip.Features.ItemTagging
+local TSK = ItemTagging.TranslatedStrings
 
 ---------------------------------------------
 -- METHODS
@@ -11,11 +12,11 @@ local ItemTagging = Epip.Features.ItemTagging
 ---@param text string
 function ItemTagging.AddTooltipFootnote(tooltip, text)
     local desc = tooltip:GetFirstElement("ItemDescription")
-    if desc then -- Use ItemDescription if available.
+    if desc then
         desc.Label = desc.Label .. "<br><br>" .. text
     else
         tooltip:InsertElement({
-            Type = "WeaponDamagePenalty",
+            Type = "ItemDescription",
             Label = text,
         })
     end
@@ -30,19 +31,16 @@ Client.Tooltip.Hooks.RenderItemTooltip:Subscribe(function (ev)
     local item = ev.Item
     local bookRead = item:HasTag(ItemTagging.BOOK_READ_TAG)
     local keyUsed = item:HasTag(ItemTagging.KEY_USED_TAG)
-    local text
+    local text = nil
 
     if bookRead then
-        text = Text.Format("Book read.", {
-            Color = "488f1f",
-        })
+        text = TSK.Hint_UsedItem_Book:GetString()
     elseif keyUsed then
-        text = Text.Format("Key used.", {
-            Color = "488f1f",
-        })
+        text = TSK.Hint_UsedItem_Key:GetString()
     end
 
     if text then
+        text = Text.Format(text, {Color = ItemTagging.USED_ITEM_HINT_COLOR}) -- Formatting is done here rather than within the TSK itself to make the color simpler to change by other scripts.
         ItemTagging.AddTooltipFootnote(ev.Tooltip, text)
     end
 end)
--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ItemTagging/Shared.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ItemTagging/Shared.lua
@@ -7,9 +7,23 @@
 local ItemTagging = {
     KEY_USED_TAG = "PIP_ITEMTAGGING_KEYUSED",
     BOOK_READ_TAG = "PIP_ITEMTAGGING_BOOKUSED",
+    USED_ITEM_HINT_COLOR = "488f1f",
 
     USE_LEGACY_EVENTS = false,
     USE_LEGACY_HOOKS = false,
+
+    TranslatedStrings = {
+        Hint_UsedItem_Book = {
+            Handle = "h47312526g9ce5g4deeg8e67gde4e20811748",
+            Text = "Book read.",
+            ContextDescription = "Tooltip hint for used books",
+        },
+        Hint_UsedItem_Key = {
+            Handle = "he782b845g749cg4c62g9095g0164424ed054",
+            Text = "Key used.",
+            ContextDescription = "Tooltip hint for used keys",
+        },
+    },
 
     Hooks = {
         IsItemUsed = {}, ---@type Event<Features.ItemTagging.Hooks.IsItemUsed>
@@ -47,5 +61,5 @@ end
 -- Default implementation for IsItemUsed.
 ItemTagging.Hooks.IsItemUsed:Subscribe(function (ev)
     local item = ev.Item
-    ev.Used = item:HasTag(ItemTagging.KEY_USED_TAG) or item:HasTag(ItemTagging.BOOK_READ_TAG)
+    ev.Used = ev.Used or item:HasTag(ItemTagging.KEY_USED_TAG) or item:HasTag(ItemTagging.BOOK_READ_TAG)
 end, {StringID = "DefaultImplementation"})
