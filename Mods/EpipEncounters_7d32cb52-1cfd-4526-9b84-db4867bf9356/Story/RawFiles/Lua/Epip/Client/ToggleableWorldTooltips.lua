57d1552e13dcc5f9c5649b2f030da630c903089b
Kamil <lekamilthemage@gmail.com>
2023-10-08 10:23:45 +0200

    ToggleableWorldTooltips: support rebinding the vanilla key, rework inner workings
    Now allows raw input events to go through, but still affects some behaviour if there are multiple events bound to the key.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/ToggleableWorldTooltips.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/ToggleableWorldTooltips.lua
@@ -1,8 +1,19 @@
 
 local Input = Client.Input
+local MessageBox = Client.UI.MessageBox
 
 local ToggleableWorldTooltips = {
+    SHOW_WORLD_TOOLTIPS_INPUTEVENTID = 280,
+    _LISTENERID_HOLD_KEY = "Features.ToggleableWorldTooltips.HoldKey",
     active = false,
+
+    TranslatedStrings = {
+        Warning_VanillaKeyNotBound = {
+            Handle = "hdaeb81edg863bg492cgab09ged3a5d2470d7",
+            Text = "Using toggleable world tooltips requires binding the vanilla key for them.",
+            ContextDescription = "Message box shown if \"Show World Tooltips\" is not bound in vanilla input menu",
+        },
+    },
 
     INPUT_ACTION = "EpipEncounters_ToggleWorldTooltips",
 }
@@ -23,22 +34,43 @@ end
 -- Toggle the feature when the action is performed.
 Client.Input.Events.ActionExecuted:Subscribe(function (ev)
     if ev.Action.ID == ToggleableWorldTooltips.INPUT_ACTION then
+        local binding = Input.GetBinding(ToggleableWorldTooltips.SHOW_WORLD_TOOLTIPS_INPUTEVENTID, "Key")
         ToggleableWorldTooltips.active = not ToggleableWorldTooltips.active
 
         -- Send an alt press/release when toggled.
-        if ToggleableWorldTooltips:IsEnabled() then
-            Input.Inject("Key", "lalt", "Pressed", 1, 1)
-        else
-            Input.Inject("Key", "lalt", "Released", 0, 0)
-        end
-    end
-end)
+        -- TODO consider modifiers!
+        if binding then
+            local numericID = Input.GetRawInputNumericID(binding.InputID)
 
--- Prevent releasing alt while the feature is toggled on.
-Input.Events.KeyStateChanged:Subscribe(function (ev)
-    if ev.InputID == "lalt" and ev.State == "Released" then
-        if ToggleableWorldTooltips:IsEnabled() then
-            ev:Prevent()
+            if ToggleableWorldTooltips:IsEnabled() then
+                Input.Inject("Key", binding.InputID, "Pressed", 1, 1)
+                GameState.Events.Tick:Subscribe(function (_)
+                    local manager = Ext.Input.GetInputManager()
+                    local keyboardState = manager.InputStates[manager.PerDeviceData[1].DeviceId + 1] -- Assumes the first device is keyboard - not 100% if this is always the case?
+
+                    local state = keyboardState.Inputs[numericID + 1]
+                    if ToggleableWorldTooltips:IsEnabled() then
+                        state.State = "Released" -- Pressed state will not work.
+                        state.Value = 1
+                        state.Value2 = 1
+                    else
+                        state.State = "Released"
+                        state.Value = 0
+                        state.Value2 = 0
+
+                        -- Release by itself is not enough to stop the tooltips from showing.
+                        Input.Inject("Key", binding.InputID, "Pressed", 1, 1)
+                        Input.Inject("Key", binding.InputID, "Released", 0, 0)
+
+                        GameState.Events.Tick:Unsubscribe(ToggleableWorldTooltips._LISTENERID_HOLD_KEY)
+                    end
+                end, {StringID = ToggleableWorldTooltips._LISTENERID_HOLD_KEY})
+            end
+        else -- Show warning that the vanilla key must be bound
+            MessageBox.Open({
+                Header = "",
+                Message = ToggleableWorldTooltips.TranslatedStrings.Warning_VanillaKeyNotBound:GetString(),
+            })
         end
     end
 end)
