8a0b3e41be094317b4e16a3ec5ddc905ce888d20
Kamil <lekamilthemage@gmail.com>
2023-12-16 10:38:14 +0100

    TooltipLib: fix regression with positioning status tooltips, consider UI scaling

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/Client/Tooltip/Status.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/Client/Tooltip/Status.lua
@@ -23,9 +23,17 @@ local Tooltip = Client.Tooltip
 ---@param options TooltipLib.FormattedTooltip.Status.CreationOptions? If not present, default values of the class will be used.
 function Tooltip.ShowStatusTooltip(status, options)
     options = options or {}
-    local ui = Tooltip._GetDefaultCustomTooltipUI()
+    local ui = Tooltip._GetCustomStatusTooltipUI()
     local mouseX, mouseY = Client.GetMousePosition()
     local position = options.Position or Vector.Create(mouseX, mouseY)
+    local scaleMultiplier = 1 / Client.UI.Tooltip:GetUI():GetUIScaleMultiplier() -- We're converting from screen space to flash space, so we'll need to multiply by 1 / scale.
 
-    ui:ExternalInterfaceCall("showStatusTooltip", Client.Flash.ToFlashHandle(status.OwnerHandle), Client.Flash.ToFlashHandle(status.StatusHandle), position[1], position[2], 0, 0, options.Align or "") -- Offset can be applied onto Position instead.
+    ui:ExternalInterfaceCall("showStatusTooltip", Client.Flash.ToFlashHandle(status.OwnerHandle), Client.Flash.ToFlashHandle(status.StatusHandle), position[1] * scaleMultiplier, position[2] * scaleMultiplier, 0, 0, options.Align or "") -- Offset can be applied onto Position instead.
+end
+
+---Returns the UI to use to display custom status tooltips.
+---Different as the PartyInventory UI seems to apply a strange offset to positioning.
+---@return UIObject
+function Tooltip._GetCustomStatusTooltipUI()
+    return Ext.UI.GetByType(Ext.UI.TypeID.containerInventory.Default)
 end
