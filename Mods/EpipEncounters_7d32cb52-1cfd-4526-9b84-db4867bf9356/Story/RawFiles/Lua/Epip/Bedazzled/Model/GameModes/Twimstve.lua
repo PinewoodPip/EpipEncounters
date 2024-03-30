59c206d234e64d6ee72c3a33a688e1a9eda5f461
Kamil <lekamilthemage@gmail.com>
2024-03-30 08:18:10 +0100

    Bedazzled Twimstve: add free moves limit

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Bedazzled/Model/GameModes/Twimstve.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Bedazzled/Model/GameModes/Twimstve.lua
@@ -1,13 +1,23 @@
 
 local Bedazzled = Epip.GetFeature("Feature_Bedazzled")
 local BoardClass = Bedazzled:GetClass("Feature_Bedazzled_Board")
+local GameClass = Bedazzled:GetClass("Features.Bedazzled.GameMode")
 local V = Vector.Create
 local GemStates = {
     MoveFrom = Bedazzled.GetGemStateClass("Features.Bedazzled.Board.Gem.State.MoveFrom")
 }
 
 ---@class Features.Bedazzled.GameModes.Twimstve : Features.Bedazzled.GameMode
+---@field FreeMovesRemaining integer
 local Game = {
+    DEFAULT_FREE_MOVES = 25,
+    FREE_MOVE_BONUSES = {
+        RUNE_CRAFT_SMALL = 2,
+        RUNE_CRAFT_LARGE = 3,
+        RUNE_CRAFT_GIANT = 4,
+        EPIPE_CONSUMED = 5,
+    },
+
     _CallistoQueuedGemTypes = {}, ---@type table<Feature_Bedazzled_Board_Gem, string[]> -- Gem types to zap when a Callisto finished being rotated.
 
     Name = Bedazzled:RegisterTranslatedString({
@@ -32,6 +42,32 @@ setmetatable(Game._CallistoQueuedGemTypes, {
 -- METHODS
 ---------------------------------------------
 
+---@override
+function Game:Create(size)
+    local board = GameClass.Create(self, size) ---@cast board Features.Bedazzled.GameModes.Twimstve
+    board.FreeMovesRemaining = self.DEFAULT_FREE_MOVES
+
+    -- Grant extra free moves when certain gems are consumed.
+    board.Events.MatchExecuted:Subscribe(function (ev)
+        for _,gem in ipairs(ev.Match:GetAllGems()) do
+            if gem.Type == "Epipe" then
+                board:AddFreeMoves(board.FREE_MOVE_BONUSES.EPIPE_CONSUMED)
+            end
+        end
+        for _,fusion in ipairs(ev.Match.Fusions) do
+            if fusion.TargetModifier == "Rune" then
+                board:AddFreeMoves(board.FREE_MOVE_BONUSES.RUNE_CRAFT_SMALL)
+            elseif fusion.TargetModifier == "LargeRune" then
+                board:AddFreeMoves(board.FREE_MOVE_BONUSES.RUNE_CRAFT_LARGE)
+            elseif fusion.TargetModifier == "GiantRune" then
+                board:AddFreeMoves(board.FREE_MOVE_BONUSES.RUNE_CRAFT_GIANT)
+            end
+        end
+    end)
+
+    return board
+end
+
 ---Attempts to rotate gems around, which counts as a move.
 ---@param anchor Vector2 Top-left gem coordinate.
 ---@param direction "Clockwise"|"Anti-clockwise"
@@ -41,29 +77,18 @@ function Game:Rotate(anchor, direction)
     end
     local gems = self:__GetAnchorGems(anchor)
 
-    -- Report an invalid move if rotation at this position is not possible.
-    if not self:CanRotateAt(anchor) then
+    -- Report an invalid move if rotation at this position is not possible, or if free moves are exhausted and this move would not result in a match.
+    if not self:CanRotateAt(anchor) or (self.FreeMovesRemaining <= 0 and not self:_WouldMoveResultInMatch(anchor, direction)) then
         self:ReportInvalidMove({
             Position = anchor,
             InteractedGems = gems,
         })
     else
-        local newStates = {}
-        local lastGem = direction == "Anti-clockwise" and gems[1] or gems[#gems]
-        local indexDirection = direction == "Anti-clockwise" and 1 or -1
-        for i=(direction == "Anti-clockwise" and 1 or #gems),(direction == "Anti-clockwise" and #gems or 1),indexDirection do
-            local nextGem = gems[i + indexDirection] or lastGem
-            newStates[i] = {Data = self:GetGemData(nextGem), Type = nextGem.Type, Modifiers = nextGem.Modifiers, OriginalPosition = V(nextGem:GetBoardPosition())}
-        end
+        local newStates = self:_RotateGemStates(anchor, direction)
 
-        -- TODO check for validity for rotate?
-        for i=1,#newStates,1 do
+        -- Queue gem types to zap if a Callisto Anomaly was involved.
+        for i=1,#gems,1 do
             local gem = gems[i]
-            local newState = newStates[i]
-            self:ApplyGemData(gem, newState.Data)
-            gem:SetState(GemStates.MoveFrom:Create(newState.OriginalPosition))
-
-            -- Queue gem types to zap if a Callisto Anomaly was involved.
             if gem.Type == "Protean" then
                 local otherTypes = {} ---@type table<Feature_Bedazzled_GemDescriptor_ID, true>
                 for _,otherGem in ipairs(gems) do
@@ -72,6 +97,29 @@ function Game:Rotate(anchor, direction)
                 otherTypes["Protean"] = nil -- Don't zap other proteans.
                 self._CallistoQueuedGemTypes[gem] = otherTypes
             end
+        end
+
+        -- Check if the move would result in a match
+        local isFreeMove = true
+        for i=1,#newStates,1 do
+            local pos = newStates[i].OriginalPosition
+            local match = self:GetMatchAt(pos:unpack())
+            if match ~= nil then
+                isFreeMove = false
+                break
+            end
+        end
+
+        -- Decrement free moves
+        if isFreeMove then
+            self.FreeMovesRemaining = self.FreeMovesRemaining - 1
+        end
+
+        for i=1,#newStates,1 do
+            local gem = gems[i]
+            local newState = newStates[i]
+            -- TODO there is an off-by-one error in this. But where?
+            gem:SetState(GemStates.MoveFrom:Create(newState.OriginalPosition - V(0, 1)))
         end
 
         self:ReportMove({
@@ -101,6 +149,12 @@ end
 function Game:IsAnchorValid(anchor)
     local size = self.Size
     return anchor[1] < size[1] and anchor[2] > 1
+end
+
+---Increments the amount of free moves available.
+---@param moves integer
+function Game:AddFreeMoves(moves)
+    self.FreeMovesRemaining = self.FreeMovesRemaining + moves
 end
 
 ---Returns the gems within the rotator at an anchor.
@@ -136,5 +190,63 @@ end
 ---@override
 ---@return boolean
 function Game:HasMovesAvailable()
-    return true -- TODO! but maybe should be true to allow free-move...
+    -- Check free moves first
+    local hasMoves = self.FreeMovesRemaining > 0
+
+    -- Otherwise check for valid moves on board
+    if not hasMoves then
+        for i=1,self.Size[1] - 1,1 do
+            for j=2,self.Size[2],1 do
+                -- TODO consider other rotation directions
+                local anchor = V(i, j)
+                if self:CanRotateAt(anchor) and self:_WouldMoveResultInMatch(anchor, "Clockwise") then
+                    hasMoves = true
+                    break
+                end
+            end
+        end
+    end
+
+    return hasMoves
+end
+
+---Swaps gem data according to rotator behaviour.
+---@param anchor Vector2 Top-left gem coordinate.
+---@param direction "Clockwise"|"Anti-clockwise"
+---@return table
+function Game:_RotateGemStates(anchor, direction)
+    local gems = self:__GetAnchorGems(anchor)
+    local newStates = {}
+    local lastGem = direction == "Anti-clockwise" and gems[1] or gems[#gems]
+    local indexDirection = direction == "Anti-clockwise" and 1 or -1
+    for i=(direction == "Anti-clockwise" and 1 or #gems),(direction == "Anti-clockwise" and #gems or 1),indexDirection do
+        local nextGem = gems[i + indexDirection] or lastGem
+        newStates[i] = {Data = self:GetGemData(nextGem), Type = nextGem.Type, Modifiers = nextGem.Modifiers, OriginalPosition = V(self:GetGemGridCoordinates(nextGem))}
+    end
+    for i=1,#newStates,1 do
+        local gem = gems[i]
+        local newState = newStates[i]
+        self:ApplyGemData(gem, newState.Data)
+    end
+    return newStates
+end
+
+---Returns whether a move would result in a match.
+---@param anchor Vector2 Top-left gem coordinate.
+---@param direction "Clockwise"|"Anti-clockwise"
+function Game:_WouldMoveResultInMatch(anchor, direction)
+    local otherDirection = direction == "Clockwise" and "Anti-clockwise" or "Clockwise"
+    self:_RotateGemStates(anchor, direction)
+    local wouldMatch = false
+    local gems = self:__GetAnchorGems(anchor)
+    for _,gem in ipairs(gems) do
+        local pos = self:GetGemGridCoordinates(gem)
+        local match = self:GetMatchAt(pos:unpack())
+        if match ~= nil then
+            wouldMatch = true
+            break
+        end
+    end
+    self:_RotateGemStates(anchor, otherDirection)
+    return wouldMatch
 end
