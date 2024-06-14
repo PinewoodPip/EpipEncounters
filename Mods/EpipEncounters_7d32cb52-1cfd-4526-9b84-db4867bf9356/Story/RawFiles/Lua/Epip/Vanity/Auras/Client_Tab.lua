e0fbdaad3fe929b77577771c0119f337b9166d3b
Kamil <lekamilthemage@gmail.com>
2024-06-14 09:32:23 +0200

    Vanity Auras: extract tab TSKs

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Vanity/Auras/Client_Tab.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Vanity/Auras/Client_Tab.lua
@@ -3,8 +3,26 @@ local Vanity = Client.UI.Vanity
 
 ---@class Feature_Vanity_Auras
 local Auras = Epip.GetFeature("Feature_Vanity_Auras")
+local TSK = {
+    VanityTab = Auras:RegisterTranslatedString({
+        Handle = "hba14c3f1g1d8fg4ddeg935bg2a8189f90d32",
+        Text = "Auras",
+        ContextDescription = [[Vanity tab name]],
+    }),
+    Label_RemoveAuras = Auras:RegisterTranslatedString({
+        Handle = "h1bbafd1cg0289g47cbga8cdg93e45e2edbdc",
+        Text = "Remove Auras",
+        ContextDescription = [[Button label]],
+    }),
+    Label_AttachmentPoint = Auras:RegisterTranslatedString({
+        Handle = "h1050422bg60f7g4cfag8a89ge22ef3bcf7c6",
+        Text = "Attachment Point",
+        ContextDescription = [[Dropdown label for where effects should be placed on the character (arms, legs, head, etc.)]],
+    }),
+}
+
 Auras.Tab = Vanity.CreateTab({
-    Name = "Auras",
+    Name = TSK.VanityTab:GetString(),
     ID = "PIP_Auras",
     Icon = "hotbar_icon_magic",
 })
@@ -31,12 +49,12 @@ function Tab:Render()
     local open = Vanity.IsCategoryOpen("Auras")
     local char = Client.GetCharacter()
 
-    Vanity.RenderButton("RemoveAuras", "Remove Auras", true)
+    Vanity.RenderButton("RemoveAuras", TSK.Label_RemoveAuras:GetString(), true)
 
-    Vanity.RenderText("_BoneLabel", "Attachment Point")
+    Vanity.RenderText("_BoneLabel", TSK.Label_AttachmentPoint:GetString())
     Vanity.RenderDropdown("Auras_Bone", Tab:_GetBoneDropdownOptions(), Tab.SelectedBoneIndex)
 
-    Vanity.RenderEntry("Auras", "Auras", true, open, false, false, nil, false, nil)
+    Vanity.RenderEntry("Auras", TSK.VanityTab:GetString(), true, open, false, false, nil, false, nil)
     if open then
         for id,effect in pairs(Auras.GetRegisteredAuras()) do
             local icon = Auras.HasAura(char, id) and "hotbar_icon_charsheet" or nil
