b44389b3ae50ec4044bfee9455509b1a2e935a16
Kamil <lekamilthemage@gmail.com>
2024-07-19 20:30:59 +0200

    InventoryMultiSelect: add support for dragging out of non-player container UIs

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/InventoryMultiSelect/MultiDragHandlers/Client.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/InventoryMultiSelect/MultiDragHandlers/Client.lua
@@ -1,5 +1,6 @@
 
 local PartyInventory = Client.UI.PartyInventory
+local ContainerInventory = Client.UI.ContainerInventory
 
 ---@class Features.InventoryMultiSelect
 local MultiSelect = Epip.GetFeature("Features.InventoryMultiSelect")
@@ -41,29 +42,44 @@ MultiSelect.Events.MultiDragEnded:Subscribe(function (ev)
         else -- Put items into hovered slot and subsequent available ones
             -- Must be done next tick, or emulating startDragging will throw "Already multi-dragging" error
             Ext.OnNextTick(function ()
+                local itemNetIDsToSend = {} ---@type NetId[] Items to request the server to send directly to the target inventory.
+                local characterNetID = nil
                 for _,inv in ipairs(MultiSelect._GetInventoryMovieClips()) do
                     if inv.currentSelection >= 0 then
+                        characterNetID = Character.Get(inv.id, true).NetID
+
+                        -- Move items to the inventory
                         local nextSlotIndex = inv.currentSelection
-
-                        -- Move each selection to the next available slot starting from hovered one
                         for _,selection in ipairs(ev.OrderedSelections) do
-                            local slot = inv.content_array[nextSlotIndex]
-                            while slot and slot._itemHandle ~= 0 do -- Search next empty slot. Cells that don't exist are surely empty.
-                                nextSlotIndex = nextSlotIndex + 1
-                                slot = inv.content_array[nextSlotIndex]
-                                if nextSlotIndex > 999 then
-                                    MultiSelect:InternalError("Events.MultiDragEnded", "Could not find slot within reasonable range")
+                            if selection.Type == "ContainerInventory" and not ContainerInventory.IsPlayerInventory() then -- Items within non-player containers cannot be mass-dragged properly as the client appears to have lesser authority. Send them to the container instead.
+                                table.insert(itemNetIDsToSend, Item.Get(selection.ItemHandle).NetID)
+                            else
+                                -- Move each selection to the next available slot starting from hovered one
+                                local slot = inv.content_array[nextSlotIndex]
+                                while slot and slot._itemHandle ~= 0 do -- Search next empty slot. Cells that don't exist are surely empty.
+                                    nextSlotIndex = nextSlotIndex + 1
+                                    slot = inv.content_array[nextSlotIndex]
+                                    if nextSlotIndex > 999 then
+                                        MultiSelect:InternalError("Events.MultiDragEnded", "Could not find slot within reasonable range")
+                                    end
                                 end
+
+                                MultiSelect._MoveItemToPartyInventorySlot(selection.ItemHandle, inv.id, nextSlotIndex)
+
+                                nextSlotIndex = nextSlotIndex + 1
                             end
-
-                            MultiSelect._MoveItemToPartyInventorySlot(selection.ItemHandle, inv.id, nextSlotIndex)
-
-                            nextSlotIndex = nextSlotIndex + 1
                         end
 
                         MultiSelect:DebugLog("Holy fuck. The title of 'UI Hackerman' is truly befitting. Anyways the items have been moved")
                         break
                     end
+                end
+                -- Request items from non-player container UIs to be sent by the server instead.
+                if itemNetIDsToSend[1] then
+                    Net.PostToServer(MultiSelect.NETMSG_SEND_TO_CHARACTER, {
+                        ItemNetIDs = itemNetIDsToSend,
+                        CharacterNetID = characterNetID,
+                    })
                 end
             end)
             PartyInventory:PlaySound(MultiSelect.SOUND_DRAG_TO_SLOT)
