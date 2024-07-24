aaebe9fa0a85dd7dc088b7e1487ea3b4f23d3846
Kamil <lekamilthemage@gmail.com>
2024-07-24 11:22:49 +0200

    SkillsUI: add UpdateSkills hook

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/UI/Skills.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/UI/Skills.lua
@@ -1,9 +1,41 @@
 
 ---@class SkillsUI : UI
 local Skills = {
+    FLASH_ENTRY_TEMPLATES = {
+        SKILL = {
+            "Index",
+            "Skill",
+            "School",
+            "Learnt",
+            "MemoryCost",
+            "Memorized",
+        },
+    },
     selectedSkill = nil, ---@type string
+
+    USE_LEGACY_EVENTS = false,
+    USE_LEGACY_HOOKS = false,
+
+    Hooks = {
+        UpdateSkills = {}, ---@type Hook<UI.Skills.Hooks.UpdateSkills>
+    },
 }
 Epip.InitializeUI(Ext.UI.TypeID.skills, "Skills", Skills)
+
+---------------------------------------------
+-- CLASSES & EVENTS
+---------------------------------------------
+
+---@class UI.Skills.Entries.Skill
+---@field Index integer
+---@field Skill skill
+---@field School integer
+---@field Learnt boolean
+---@field MemoryCost integer
+---@field Memorized boolean
+
+---@class UI.Skills.Hooks.UpdateSkills
+---@field Entries UI.Skills.Entries.Skill Hookable.
 
 ---------------------------------------------
 -- METHODS
@@ -28,7 +60,20 @@ end
 Skills:RegisterCallListener("showSkillTooltip", function(_, _, skillID)
     Skills.selectedSkill = skillID
 end)
-
 Skills:RegisterCallListener("hideTooltip", function(_)
     Skills.selectedSkill = nil
+end)
+
+-- Hook skill updates.
+Skills:RegisterInvokeListener("updateSkills", function (ev, _) -- Boolean param purpose is unknown.
+    local root = ev.UI:GetRoot()
+    local array = root.skillsUpdateList
+    local TEMPLATE = Skills.FLASH_ENTRY_TEMPLATES.SKILL
+    local entries = Client.Flash.ParseArray(array, TEMPLATE)
+
+    entries = Skills.Hooks.UpdateSkills:Throw({
+        Entries = entries,
+    }).Entries
+
+    Client.Flash.EncodeArray(array, TEMPLATE, entries)
 end)
