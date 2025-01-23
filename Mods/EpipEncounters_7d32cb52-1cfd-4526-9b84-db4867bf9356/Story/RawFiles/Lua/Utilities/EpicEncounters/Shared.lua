21a58e620df1f493de61829812c4f6c5b816abde
Kamil <lekamilthemage@gmail.com>
2025-01-23 10:21:29 +0100

    EpicEncountersLib: add TSKs for embodiment names

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/EpicEncounters/Shared.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Utilities/EpicEncounters/Shared.lua
@@ -1,6 +1,34 @@
 
 ---@class EpicEncountersLib : Library
-EpicEncounters = {}
+EpicEncounters = {
+    TranslatedStrings = {
+        Embodiment_Force = {
+            Handle = "h53c27242gdc58g43f2g999fg1305a5e6aeaa",
+            Text = "Force",
+            ContextDescription = [[Embodiment name]],
+        },
+        Embodiment_Entropy = {
+            Handle = "h53d49f75g5520g48e1g8fc0gb793134dd234",
+            Text = "Entropy",
+            ContextDescription = [[Embodiment name]],
+        },
+        Embodiment_Form = {
+            Handle = "haa562285gecd2g41efg9db6gb3aa2b0b32b3",
+            Text = "Form",
+            ContextDescription = [[Embodiment name]],
+        },
+        Embodiment_Inertia = {
+            Handle = "hca631646g887ag4958g8efcgc0adb2cb7969",
+            Text = "Inertia",
+            ContextDescription = [[Embodiment name]],
+        },
+        Embodiment_Life = {
+            Handle = "hd8b2a948g5b60g41dfgae0eg4efb456d8c24",
+            Text = "Life",
+            ContextDescription = [[Embodiment name]],
+        },
+    },
+}
 Epip.InitializeLibrary("EpicEncounters", EpicEncounters)
 
 ---------------------------------------------
