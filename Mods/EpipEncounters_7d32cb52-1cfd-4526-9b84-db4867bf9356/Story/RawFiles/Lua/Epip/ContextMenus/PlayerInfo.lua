6109d8f708ecd7fd629bf0c21b904da1ccee130c
Kamil <lekamilthemage@gmail.com>
2025-06-21 19:21:51 +0200

    PlayerInfo: extract TSKs for context menu

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ContextMenus/PlayerInfo.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/ContextMenus/PlayerInfo.lua
@@ -5,8 +5,30 @@
 ---------------------------------------------
 
 local PlayerInfo = Client.UI.PlayerInfo
-
 local cheatMenu = Debug.CheatsContextMenu
+
+---@type Feature
+local PlayerInfoContextMenu = {
+    TranslatedStrings = {
+        Label_Header = {
+            Handle = "he60622c5g07adg4b95g9df4g673e55c5d33a",
+            Text = "—— Player Status ——",
+            ContextDescription = [[Header shown in context menu when right-clicking a player portrait.]],
+        },
+        Label_ShowStatuses = {
+            Handle = "h58d447e9gf7e7g4299gbae7gbc1c9e70adda",
+            Text = "Show Statuses",
+            ContextDescription = [[Option in context menu]],
+        },
+        Label_ShowSummons = {
+            Handle = "hd8528c79g1e8ag48f3g8241g38332e78cc95",
+            Text = "Show Summons",
+            ContextDescription = [[Option in context menu]],
+        },
+    }
+}
+Epip.RegisterFeature("PlayerInfoContextMenu", PlayerInfoContextMenu)
+local TSK = PlayerInfoContextMenu.TranslatedStrings
 
 if not Epip.IsDeveloperMode() then cheatMenu = nil end
 
@@ -21,12 +43,12 @@ Ext.Events.UICall:Subscribe(function (ev) -- Very hasty workaround for not havin
 end, {Priority = 99999})
 Client.UI.ContextMenu.RegisterMenuHandler("playerInfoPlayerPortrait", function()
     local data = {
-        {id = "playerInfo_Header", type = "header", text = "—— Player Status ——"},
+        {id = "playerInfo_Header", type = "header", text = TSK.Label_Header:GetString()},
 
         -- openCharInventory no longer works in engine, it seems.
         -- {id = "playerInfo_OpenInventory", type = "button", text = "Open Inventory"},
-        {id = "playerInfo_ToggleStatuses", type = "checkbox", text = "Show Statuses", checked = PlayerInfo.GetStatusesVisibility()},
-        {id = "playerInfo_ToggleSummons", type = "checkbox", text = "Show Summons", checked = PlayerInfo.GetSummonsVisibility()},
+        {id = "playerInfo_ToggleStatuses", type = "checkbox", text = TSK.Label_ShowStatuses:GetString(), checked = PlayerInfo.GetStatusesVisibility()},
+        {id = "playerInfo_ToggleSummons", type = "checkbox", text = TSK.Label_ShowSummons:GetString(), checked = PlayerInfo.GetSummonsVisibility()},
 
         cheatMenu,
     }
@@ -50,7 +72,7 @@ end)
 -- Same as above, but for summons - toggling statuses/hiding summons makes no sense for them
 Client.UI.ContextMenu.RegisterMenuHandler("playerInfoSummonPortrait", function()
     local data = {
-        {id = "playerInfo_Header", type = "header", text = "—— Player Status ——"},
+        {id = "playerInfo_Header", type = "header", text = TSK.Label_Header:GetString()},
 
         cheatMenu,
     }
