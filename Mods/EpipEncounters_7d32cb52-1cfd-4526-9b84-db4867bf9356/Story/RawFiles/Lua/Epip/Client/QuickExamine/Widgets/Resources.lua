beb36a0a52c2a129c57e8ce3272075ea25cd8938
Kamil <lekamilthemage@gmail.com>
2026-01-05 15:56:30 +0100

    QuickExamine: show movement distance, make resources more compact
    Also added some more fitting character sheet icons to go along with it.

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/QuickExamine/Widgets/Resources.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/QuickExamine/Widgets/Resources.lua
@@ -7,11 +7,14 @@ local BH = EpicEncounters.BatteredHarried
 
 ---@class Features.QuickExamine.Widgets.Resources : Feature
 local BasicInfo = {
+    TSKHANDLE_MOVEMENT_DISTANCE = "hd589d36bg8be6g49ceg90adg6b6b338fa76d", -- "[1]m"
+
     ICON_SIZE = 22,
     RESOURCE_TEXT_SIZE = 32,
     AP_ICON = "ui_24x_action_point",
     SP_ICON = "ui_24x_source_point",
-    INITIATIVE_ICON = "ui_24x_initiative",
+    INITIATIVE_ICON = "PIP_UI_Icon_CharacterSheet_Initiative",
+    MOVEMENT_ICON = "PIP_UI_Icon_CharacterSheet_Movement",
 }
 Epip.RegisterFeature("Features.QuickExamine.Widgets.Resources", BasicInfo)
 
@@ -49,7 +52,8 @@ function Widget:Render(entity)
             Character.GetActionPoints(entity)
         }
     })
-    LabelledIcon.Create(QuickExamine.UI, "Resources_AP", horizontalList, BasicInfo.AP_ICON, apLabel, Vector.Create(ICON_SIZE, ICON_SIZE), Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE))
+    local apLabelledIcon = self:_RenderStat(horizontalList, "Resources_AP", BasicInfo.AP_ICON, apLabel)
+    apLabelledIcon.Text:FitSize()
 
     -- Source points
     local spLabel = Text.Format("%s/%s", {
@@ -57,10 +61,17 @@ function Widget:Render(entity)
             Character.GetSourcePoints(entity),
         }
     })
-    LabelledIcon.Create(QuickExamine.UI, "Resources_SP", horizontalList, BasicInfo.SP_ICON, spLabel, Vector.Create(ICON_SIZE, ICON_SIZE), Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE))
+    local spLabelledIcon = self:_RenderStat(horizontalList, "Resources_SP", BasicInfo.SP_ICON, spLabel)
+    spLabelledIcon.Text:FitSize()
 
     -- Initiative
-    LabelledIcon.Create(QuickExamine.UI, "Resources_Initiative", horizontalList, BasicInfo.INITIATIVE_ICON, tostring(char.Stats.Initiative), Vector.Create(ICON_SIZE, ICON_SIZE), Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE))
+    local initiativeLabel = self:_RenderStat(horizontalList, "Resources_Initiative", BasicInfo.INITIATIVE_ICON, tostring(char.Stats.Initiative))
+    initiativeLabel.Text:FitSize()
+
+    -- Movement
+    local movement = Text.Round(Character.GetMovement(char) / 100, 2)
+    local movementLabel = self:_RenderStat(horizontalList, "Resources_Movement", BasicInfo.MOVEMENT_ICON, Text.FormatLarianTranslatedString(BasicInfo.TSKHANDLE_MOVEMENT_DISTANCE, movement))
+    movementLabel.Text:FitSize()
 
     -- Battered & Harried
     if EpicEncounters.IsEnabled() then
@@ -74,9 +85,25 @@ function Widget:Render(entity)
 
         -- Battered/Harried damage buffered
         local bhProgress = Text.Format("%s%%", {FormatArgs = {Text.Round(EpicEncounters.BatteredHarried.GetBufferedDamage(char) * 100, 2)}})
-        TextPrefab.Create(UI, "BasicInfo_BHProgress", horizontalList, bhProgress, "Left", Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE)) 
+        TextPrefab.Create(UI, "BasicInfo_BHProgress", horizontalList, bhProgress, "Left", Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE))
     end
 
     horizontalList:RepositionElements()
     verticalList:RepositionElements()
+end
+
+---Renders a labelled stat into the widget.
+---@param parent GenericUI_ParentIdentifier
+---@param id string
+---@param icon icon
+---@param valueLabel string
+---@return GenericUI_Prefab_LabelledIcon
+function Widget:_RenderStat(parent, id, icon, valueLabel)
+    local ICON_SIZE = BasicInfo.ICON_SIZE
+    if #valueLabel <= 2 then
+        valueLabel = valueLabel .. " " -- Workaround for FitSize() cutting off very short text horizontally.
+    end
+    local element = LabelledIcon.Create(QuickExamine.UI, id, parent, icon, valueLabel, Vector.Create(ICON_SIZE, ICON_SIZE), Vector.Create(64, BasicInfo.RESOURCE_TEXT_SIZE))
+    element.Text:FitSize()
+    return element
 end
