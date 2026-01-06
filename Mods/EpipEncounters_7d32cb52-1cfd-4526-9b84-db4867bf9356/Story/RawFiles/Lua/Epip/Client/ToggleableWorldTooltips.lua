e6a9732e941aa694ac5e88752414d18acb58173c
Kamil <lekamilthemage@gmail.com>
2026-01-06 17:10:35 +0100

    ToggleableWorldTooltips: fix stack overflow if both the vanilla & toggle keybinds are bound to the same keys

--- Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/ToggleableWorldTooltips.lua
+++ Mods/EpipEncounters_7d32cb52-1cfd-4526-9b84-db4867bf9356/Story/RawFiles/Lua/Epip/Client/ToggleableWorldTooltips.lua
@@ -8,6 +8,7 @@ local ToggleableWorldTooltips = {
     active = false,
     _WasInDialogue = false,
     _ActiveUponEnteringDialogue = false,
+    _IgnoreToggleRequests = false, -- Necessary to prevent stack overflows if the user binds both the vanilla keybind and the toggle keybind to the same key.
 
     TranslatedStrings = {
         Warning_VanillaKeyNotBound = {
@@ -33,9 +34,12 @@ end
 
 ---Activates world tooltips.
 function ToggleableWorldTooltips._Activate()
+    if ToggleableWorldTooltips._IgnoreToggleRequests then return end
     local binding = Input.GetBinding(ToggleableWorldTooltips.SHOW_WORLD_TOOLTIPS_INPUTEVENTID, "Key")
 
+    ToggleableWorldTooltips._IgnoreToggleRequests = true -- Prevent re-entry if both the vanilla & toggle keybinds are bound to the same key.
     Input.Inject("Key", binding.InputID, "Pressed", 1, 1)
+    ToggleableWorldTooltips._IgnoreToggleRequests = false
 
     GameState.Events.Tick:Subscribe(function (_)
         local manager = Ext.Input.GetInputManager()
@@ -57,6 +61,7 @@ end
 
 ---Deactivates world tooltips.
 function ToggleableWorldTooltips._Deactivate()
+    if ToggleableWorldTooltips._IgnoreToggleRequests then return end
     local manager = Ext.Input.GetInputManager()
     local keyboardState = manager.InputStates[manager.PerDeviceData[1].DeviceId + 1]
     local binding = Input.GetBinding(ToggleableWorldTooltips.SHOW_WORLD_TOOLTIPS_INPUTEVENTID, "Key")
@@ -67,9 +72,12 @@ function ToggleableWorldTooltips._Deactivate()
     state.Value = 0
     state.Value2 = 0
 
-    -- Release by itself is not enough to stop the tooltips from showing.
+    -- Release by itself is not enough to stop the tooltips from showing;
+    -- we must inject the press event as well.
+    ToggleableWorldTooltips._IgnoreToggleRequests = true -- Prevent re-entry if both the vanilla & toggle keybinds are bound to the same key.
     Input.Inject("Key", binding.InputID, "Pressed", 1, 1)
     Input.Inject("Key", binding.InputID, "Released", 0, 0)
+    ToggleableWorldTooltips._IgnoreToggleRequests = false
 
     ToggleableWorldTooltips.active = false
 end
